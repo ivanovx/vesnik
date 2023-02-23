5a7497323628cbaa37a1429ff0fd8fe038dd9c94
Ivan Ivanov <me@ivanov.pro>
2023-02-23 21:06:15 +0200

    refactor packages

--- src/main/java/pro/ivanov/Parser.java
+++ src/main/java/pro/ivanov/Parser.java
@@ -3,42 +3,21 @@ package pro.ivanov;
 import lombok.SneakyThrows;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
-import org.apache.tika.parser.pdf.PDFParser;
-import org.apache.tika.parser.pdf.PDFParserConfig;
 import org.apache.tika.sax.BodyContentHandler;
-import org.apache.tika.sax.ToXMLContentHandler;
-import org.xml.sax.ContentHandler;
 
 import java.io.*;
+import java.nio.file.Path;
 
 public class Parser {
-    private final String fileName;
-
-    public Parser(String fileName) {
-        this.fileName = fileName;
-    }
 
     @SneakyThrows
-    public String toText() {
-        PDFParser pdfParser = new PDFParser();
-        PDFParserConfig config = new PDFParserConfig();
+    public static String toText(Path path) {
+        final BodyContentHandler handler = new BodyContentHandler(-1);
+        final AutoDetectParser parser = new AutoDetectParser();
+        final Metadata metadata = new Metadata();
 
-        config.setExtractActions(true);
-        config.setExtractFontNames(false);
-        config.setExtractBookmarksText(true);
-        config.setExtractInlineImages(false);
-
-        pdfParser.setPDFParserConfig(config);
-
-        ContentHandler handler = new ToXMLContentHandler();
-        //BodyContentHandler handler = new BodyContentHandler(-1);
-        //ContentHandler handler = new BodyContentHandler(
-         //       new ToXMLContentHandler());
-        //AutoDetectParser parser = new AutoDetectParser();
-        Metadata metadata = new Metadata();
-
-        try (InputStream stream = new FileInputStream(this.fileName)) {
-            pdfParser.parse(stream, handler, metadata);
+        try (InputStream stream = new FileInputStream(path.toFile())) {
+            parser.parse(stream, handler, metadata);
 
             return handler.toString();
         }
--- src/main/java/pro/ivanov/web/Application.java
+++ src/main/java/pro/ivanov/web/Application.java
@@ -1,32 +1,76 @@
 package pro.ivanov.web;
 
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.TextField;
+import lombok.SneakyThrows;
+
+import opennlp.tools.tokenize.SimpleTokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import pro.ivanov.Indexer;
+
 import pro.ivanov.Parser;
 import pro.ivanov.Tokenizer;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer implements ApplicationRunner {
 
+    @SneakyThrows
+    private static Stream<String> indexPdfContents(String path) {
+        Path rootPath = Paths.get(path);
+
+        Stream<String> contents = Files
+            .walk(rootPath)
+            .filter(Files::isRegularFile)
+            .parallel()
+            .map(file -> {
+                String text = Parser.toText(file);
+                String content = new Tokenizer(text).getContent();
+
+                return content;
+            });
+
+        return contents;
+    }
+
     public static void main(String[] args) throws IOException, ParseException {
-        SpringApplication.run(Application.class, args);
-     //Path vesnikRoot = Paths.get("C:\\Users\\csynt\\Desktop\\vesnik");
-       //List<Path> vesnikBroeve = Files.walk(vesnikRoot).filter(Files::isRegularFile).toList();
+        String path = "C:\\Users\\csynt\\Desktop\\vesnik";
+
+         Stream<String> pdfContents = indexPdfContents(path);
+
+         pdfContents.forEach(content -> {
+             try {
+                 InputStream modelIn = new FileInputStream("C:\\Users\\csynt\\Desktop\\lucene-vesnik\\src\\main\\resources\\langdetect-183.bin");
+
+                 TokenizerModel model = new TokenizerModel(modelIn);
+                 TokenizerME tokenizer = new TokenizerME(model);
+
+                 System.out.println(Arrays.toString(tokenizer.tokenize(content)));
+             } catch (FileNotFoundException e) {
+                 throw new RuntimeException(e);
+             } catch (IOException e) {
+                 throw new RuntimeException(e);
+             }
+
+
+         });
+
+        //SpringApplication.run(Application.class, args);
+
+
 
       // Indexer indexer = new Indexer();
 
